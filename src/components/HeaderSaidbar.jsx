98ba5518ac35a56c3b8bd02b57d9a57bdc1f86a7
kamyarkamali <kamyarkamali021@gmail.com>
2024-07-03 19:16:39 +0330

    create project

--- src/components/HeaderSaidbar.jsx
+++ src/components/HeaderSaidbar.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 function HeaderSaidbar() {
-  return <div className="bg-red-500">HeaderSaidbar</div>;
+  return (
+    <div className="bg-[#ECE9D8]">
+      <div className="text-[10px] flex items-center gap-3 bg-[#ECE9D8] p-1 relative">
+        <button className="border-[1px] bg-[#F9F8F4] p-1 rounded-md ">
+          آموزش
+        </button>
+        <button className="border-[1px] bg-[#F9F8F4] p-1 rounded-md">
+          پژوهش
+        </button>
+        <button className="border-[1px] bg-[#F9F8F4] p-1 rounded-md">
+          دانشجویی
+        </button>
+        <button className="border-[1px] bg-[#F9F8F4] p-1 rounded-md">
+          مالی و اداری
+        </button>
+        <button className="border-[1px] bg-[#F9F8F4] p-1 rounded-md">
+          پیشخوان خدمات
+        </button>
+        <button className="border-[1px] bg-[#F9F8F4] p-1 rounded-md">
+          ثبت نام
+        </button>
+        <button className="border-[1px] bg-[#F9F8F4] p-1 rounded-md">
+          ارزشیابی
+        </button>
+        <button className="border-[1px] bg-[#F9F8F4] p-1 rounded-md">
+          سیستم
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default HeaderSaidbar;
