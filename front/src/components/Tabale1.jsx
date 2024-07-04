98b1742468deb759689673ad9cfc17d51cbd1b03
kamyarkamali <kamyarkamali021@gmail.com>
2024-07-04 10:06:08 +0330

    create tabale

--- front/src/components/Tabale1.jsx
+++ front/src/components/Tabale1.jsx
@@ -2,33 +2,43 @@ import React from "react";
 
 function Tabale1() {
   return (
-    <table className="border-[1px] border-gray-600 w-[500px]">
-      <tr className="border-[1px] border-gray-600">
-        <th className="bg-[#ECE9D8] border-l-[1px] border-gray-400 w-[5%] text-[11px]">
-          ردیف
-        </th>
-        <th className="bg-[#ECE9D8] w-[5%] border-l-[1px] border-gray-600">
-          ترم
-        </th>
-        <th className="bg-[#ECE9D8] w-[20%] border-l-[1px] border-gray-600">
-          وضع نوع ترم
-        </th>
-        <th className="bg-[#ECE9D8] w-[20%] border-l-[1px] border-gray-600">
-          3
-        </th>
-      </tr>
-      <tr>
-        <td className="text-center">1</td>
-        <td className="text-center">4021</td>
-        <td className="text-center" rowSpan="1">
-          مشعول به تحصیل - عادی
-        </td>
-      </tr>
-      <tr>
-        <td className="text-center">2</td>
-        <td className="text-center">4022</td>
-        <td className="text-center">مشغول به تحصيل - عادی</td>
-      </tr>
+    <table className="border-[1px] border-gray-600 w-[400px]">
+      <thead>
+        <tr className="border-b-[1px] border-gray-600">
+          <th className="text-center border-l-[1px] bg-[#ECE9D8] border-gray-600 w-[40px]">
+            ردیف
+          </th>
+          <tr>
+            <th className="w-[50px] border-l-[1px] border-gray-600 bg-[#ECE9D8]">
+              ترم
+            </th>
+
+            <th className="border-l-[1px] border-gray-600 w-[140px] h-[30px] bg-[#ECE9D8]">
+              وضع نوع ترم
+            </th>
+          </tr>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className="border-b-[1px] border-gray-600">
+          <th>1</th>
+
+          <tr>
+            <td className=" text-center w-[40px]">4022</td>
+            <td className=" text-center w-[170px]">مشغول به تحصیل - عادی</td>
+          </tr>
+        </tr>
+
+        <tr>
+          <th>2</th>
+          <tr>
+            <td className="text-center w-[40px]">4022</td>
+            <td className="text-center w-[170px] h-[19px]">
+              مشغول به تحصیل - عادی
+            </td>
+          </tr>
+        </tr>
+      </tbody>
     </table>
   );
 }
