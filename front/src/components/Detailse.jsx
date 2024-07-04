06d2bb36091c6b295009d21f0bcf9d3ca03cb313
kamyarkamali <kamyarkamali021@gmail.com>
2024-07-04 11:15:10 +0330

    create tabale

--- front/src/components/Detailse.jsx
+++ front/src/components/Detailse.jsx
@@ -5,6 +5,21 @@ import arrowup from "../assets/images/arrowup.png";
 import quastion from "../assets/images/quastion.jpg";
 import Tabale1 from "./Tabale1";
 
+import tick2 from "../assets/images/tick2.png";
+import pey from "../assets/images/pey.png";
+import pey2 from "../assets/images/pey2.png";
+import pey4 from "../assets/images/pey4.png";
+
+import pey3 from "../assets/images/pey3.png";
+import pey5 from "../assets/images/pey5.png";
+import pey6 from "../assets/images/pey6.png";
+import pey7 from "../assets/images/pey7.png";
+
+import pey8 from "../assets/images/pey8.png";
+import pey10 from "../assets/images/pey10.png";
+
+import pey11 from "../assets/images/pey11.png";
+
 function Detailse() {
   const [number, setNumber] = useState(402461081);
 
@@ -120,14 +135,82 @@ function Detailse() {
         </div>
       </div>
 
-      <div className="text-[11px]">
-        <span className="bg-red-200 p-1">
+      <div className="text-[13px]">
+        <span>
           {" "}
           برای مشاهده جزئیات بیشتر روی نوشته های زیر خط دار کلیک کنید
         </span>
       </div>
 
-      <div>left</div>
+      <div className="ml-[-5px]">
+        <img src={tick2} alt="/" />
+      </div>
+
+      <div className="absolute flex items-center gap-3 bottom-[-5.9rem]  right-4 border-[1px] w-fit border-gray-700">
+        <div className="flex items-center text-[12px]">
+          <p>معدل دانشگاه:</p>
+          <p>14.80</p>
+        </div>
+
+        <div className="flex items-center text-[12px] ">
+          <p>معدل دانشکده:</p>
+          <p>14.82</p>
+        </div>
+
+        <div className="flex items-center text-[12px] ">
+          <p>معدل رشته:</p>
+          <p>14.82</p>
+        </div>
+      </div>
+
+      <div className="absolute flex gap-3 items-center bottom-[-6rem] right-[22rem]">
+        <img
+          src={pey}
+          alt="/"
+          className="w-[55px] h-[29px] cursor-pointer hover:border-yellow-400"
+        />
+        <img
+          src={pey2}
+          alt="/"
+          className="w-[55px] h-[27px] cursor-pointer hover:border-yellow-400"
+        />
+        <img
+          src={pey3}
+          alt="/"
+          className="w-[55px] h-[24px] cursor-pointer hover:border-yellow-400"
+        />
+        <img
+          src={pey4}
+          alt="/"
+          className="w-[55px] h-[24px] cursor-pointer hover:border-yellow-400"
+        />
+        <img
+          src={pey5}
+          alt="/"
+          className="w-[55px] h-[24px] cursor-pointer hover:border-yellow-400"
+        />
+        <img
+          src={pey7}
+          alt="/"
+          className="w-[55px] h-[27px] cursor-pointer hover:border-yellow-400"
+        />
+      </div>
+
+      <div className="absolute bg-[#ECE9D8] bottom-[-8rem] z-30 flex items-center gap-3">
+        <img className="w-[50px] h-[23px]" src={pey8} alt="/" />
+        <img className="w-[52px] h-[20px]" src={pey10} alt="/" />
+      </div>
+
+      <div className="bottom-[-8rem] z-20 absolute left-4">
+        <img className="w-[50px]" src={pey11} alt="" />
+      </div>
+
+      <div className="bg-green-800 flex items-center gap-[10rem] absolute bottom-[-9.6rem] z-20 w-full left-0">
+        <p className="bg-green-800 text-white text-sm">1 پیغام</p>
+        <p className="bg-green-800  text-[12px] text-white">
+          جستجوي اطلاعات دانشجو انجام شد
+        </p>
+      </div>
     </div>
   );
 }
